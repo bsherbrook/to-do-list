c7042c9eafb92b73188a0cde232ffbdea72906a6
bsherbrook <bsherbrook@gmail.com>
2023-05-03 17:30:34 -0400

    link styles page

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
+import './style.scss';
 const header=document.getElementById('header')
 const contentBody= document.getElementById('content');
 const footer= document.getElementById('footer');
 const sidebar= document.getElementById('sidebar');
+
