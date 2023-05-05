7a4e31f2794b6d9c312e7e6d4679afb108fda9ed
bsherbrook <bsherbrook@gmail.com>
2023-05-05 18:50:40 -0400

    add sidebar menu display functionality

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
 import './style.scss';
 const header=document.getElementById('header')
 const contentBody= document.getElementById('content');
+const contentCard= document.getElementById('content-card');
 const footer= document.getElementById('footer');
 const sidebar= document.getElementById('sidebar');
+const menuButton= document.getElementById('drop-down');
 
+//open and close menu sidebar
+let menuSwitch=true;
+menuButton.addEventListener('click', ()=>{
+  if (menuSwitch===true){
+    contentCard.style.gridColumn='1/3';
+    sidebar.style.display='none'
+    menuSwitch= false;
+  }
+  else{
+    contentCard.style.gridColumn='';
+    sidebar.style.display='flex'
+    menuSwitch= true;
+  }
+})
