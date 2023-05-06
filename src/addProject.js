6adc48855045a6ec7633c8403f48e4344657ee11
bsherbrook <bsherbrook@gmail.com>
2023-05-06 14:23:41 -0400

    build add note modal form

--- src/addProject.js
+++ src/addProject.js
@@ -1,7 +1,9 @@
 const formBox= document.getElementById('formBox');
+const noteBox= document.getElementById('noteBox');
 let addProjSwitch= true;
-
+let addNoteSwitch=true;
 export function addProj(){
+    if (addNoteSwitch===false){addNote()}
     if (addProjSwitch=== true){
         formBox.style.transition='opacity .6s ease-in';
         formBox.style.opacity='100%';
@@ -10,5 +12,17 @@ export function addProj(){
     else{
         formBox.style.opacity='0';
         addProjSwitch=true;
+    }
+}
+export function addNote(){
+    if(addProjSwitch===false){addProj()}
+    if (addNoteSwitch===true){
+        noteBox.style.transition='opacity .6s ease-in';
+        noteBox.style.opacity='100%';
+        addNoteSwitch=false;
+    }
+    else{
+        noteBox.style.opacity='0';
+        addNoteSwitch=true;
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import './style.scss';
 import {sidebarDisplay} from './menuButton';
-import { addProj } from './addProject';
+import { addProj, addNote} from './addProject';
 const header=document.getElementById('header')
 const contentBody= document.getElementById('content');
 const contentCard= document.getElementById('content-card');
 const footer= document.getElementById('footer');
 const sidebar= document.getElementById('sidebar');
 const menuButton= document.getElementById('drop-down');
-const addProject= document.getElementById('addProject');
-const addNote= document.getElementById('addNote');
+const addProject= document.getElementById('addProjectButton');
+const addNoteButton= document.getElementById('addNoteButton');
+
 
 menuButton.addEventListener('click', sidebarDisplay);
 addProject.addEventListener('click', addProj);
+addNoteButton.addEventListener('click', addNote);
