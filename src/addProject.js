17c65ee231e5dc5492f3ed0b9e56c01048cfe127
bsherbrook <bsherbrook@gmail.com>
2023-05-09 16:21:27 -0400

    build siderbar folder add buttons

--- src/addProject.js
+++ src/addProject.js
@@ -9,7 +9,7 @@ export function addProj() {
   }
   if (addProjSwitch === true) {
     formBox.style.transition = "opacity .6s ease-in";
-    formBox.style.display='flex'; 
+    formBox.style.display = "flex";
     formBox.style.opacity = "100%";
     formBox.style.zIndex = "1";
     addProjSwitch = false;
@@ -19,8 +19,8 @@ export function addProj() {
     addProjSwitch = true;
     document.getElementById("projectTitle").value = "";
     document.getElementById("projectDescription").value = "";
-    document.getElementById("dueDate").value= "";
-    document.getElementById("priority").checked= false;
+    document.getElementById("dueDate").value = "";
+    document.getElementById("priority").checked = false;
   }
 }
 //Note form appears
@@ -31,11 +31,11 @@ export function addNote() {
   if (addNoteSwitch === true) {
     noteBox.style.transition = "opacity .6s ease-in";
     noteBox.style.opacity = "100%";
-    noteBox.style.zIndex='1';
+    noteBox.style.zIndex = "1";
     addNoteSwitch = false;
   } else {
     noteBox.style.opacity = "0";
-    noteBox.style.zIndex='-1';
+    noteBox.style.zIndex = "-1";
     addNoteSwitch = true;
     document.getElementById("noteTitle").value = "";
     document.getElementById("noteDescription").value = "";
@@ -43,7 +43,7 @@ export function addNote() {
 }
 //collect Note data-- must require before array push
 let myNotepad = [];
-export {myNotepad};
+export { myNotepad };
 export function submitNote(event) {
   let noteTitle = document.getElementById("noteTitle").value;
   let noteDescription = document.getElementById("noteDescription").value;
@@ -58,7 +58,7 @@ export function submitNote(event) {
 }
 //collect Project Data--must require certain info
 let myProjects = [];
-export {myProjects}
+export { myProjects };
 
 export function submitProject(event) {
   let projectTitle = document.getElementById("projectTitle").value;
@@ -87,6 +87,73 @@ export function submitProject(event) {
   event.preventDefault();
   addProj();
   console.log(myProjects);
+}
+export function buildNewFolder() {
+  const newFolderBox = document.getElementById("newFolderBox");
+  if (newFolderBox) {
+    return;
+  }
+  const addProjectButton = document.getElementById("addProjectButton");
+  const li = document.createElement("li");
+  const div = document.createElement("div");
+  div.setAttribute("id", "newFolderBox");
+  const input = document.createElement("input");
+  input.setAttribute("id", "newFolder");
+  input.setAttribute("type", "text");
+  input.setAttribute("autofocus", "");
+  input.setAttribute("placeholder", "Enter Project Name");
+  const buttonContainer = document.createElement("div");
+  buttonContainer.setAttribute("id", "newFolderButtons");
+  const addButton = document.createElement("button");
+  addButton.textContent = "Add";
+  addButton.setAttribute("id", "addFolderButton");
+  const deleteButton = document.createElement("button");
+  deleteButton.setAttribute("id", "deleteFolderButton");
+  deleteButton.textContent = "X";
+
+  // append the elements to the correct parents
+  const projectList = document.getElementById("projectList");
+  li.appendChild(div);
+  div.appendChild(input);
+  div.appendChild(buttonContainer);
+  buttonContainer.appendChild(addButton);
+  buttonContainer.appendChild(deleteButton);
+  projectList.insertBefore(
+    li,
+    projectList.children[projectList.children.length - 1]
+  );
+}
+export function buildNoteFolder() {
+  const newNoteBox = document.getElementById("newFolderBox");
+  if (newNoteBox) {
+    return;
+  }
+  const addNoteButton = document.getElementById("addNoteButton");
+  const li = document.createElement("li");
+  const div = document.createElement("div");
+  div.setAttribute("id", "newFolderBox");
+  const input = document.createElement("input");
+  input.setAttribute("id", "newFolder");
+  input.setAttribute("type", "text");
+  input.setAttribute("autofocus", "");
+  input.setAttribute("placeholder", "Enter Folder Name");
+  const buttonContainer = document.createElement("div");
+  buttonContainer.setAttribute("id", "newFolderButtons");
+  const addButton = document.createElement("button");
+  addButton.textContent = "Add";
+  addButton.setAttribute("id", "addFolderButton");
+  const deleteButton = document.createElement("button");
+  deleteButton.setAttribute("id", "deleteFolderButton");
+  deleteButton.textContent = "X";
+
+  // append the elements to the correct parents
+  const noteList = document.getElementById("noteList");
+  li.appendChild(div);
+  div.appendChild(input);
+  div.appendChild(buttonContainer);
+  buttonContainer.appendChild(addButton);
+  buttonContainer.appendChild(deleteButton);
+  noteList.insertBefore(li, noteList.children[noteList.children.length - 1]);
 }
 //create an array to store all of the newly created arrays in
 // const arrayHolder=[[{title:etc.},{another to do item}],[another project of todo objects]]
--- src/index.js
+++ src/index.js
@@ -1,23 +1,31 @@
-import './style.scss';
-import {sidebarDisplay} from './menuButton';
-import { addProj, addNote, submitNote, submitProject} from './addProject';
-import { displayMyProject } from './displayProjects';
-const header=document.getElementById('header')
-const contentBody= document.getElementById('content');
-const contentCard= document.getElementById('content-card');
-const footer= document.getElementById('footer');
-const sidebar= document.getElementById('sidebar');
-const menuButton= document.getElementById('drop-down');
-const addProject= document.getElementById('addProjectButton');
-const addNoteButton= document.getElementById('addNoteButton');
-const submitProjectButton= document.getElementById('submitProject');
-const submitNoteButton= document.getElementById('submitNote');
-const newToDo= document.getElementById('addToDoItem');
+import "./style.scss";
+import { sidebarDisplay } from "./menuButton";
+import {
+  addProj,
+  addNote,
+  submitNote,
+  submitProject,
+  buildNewFolder,
+  buildNoteFolder
+} from "./addProject";
+import { displayMyProject } from "./displayProjects";
+const header = document.getElementById("header");
+const contentBody = document.getElementById("content");
+const contentCard = document.getElementById("content-card");
+const footer = document.getElementById("footer");
+const sidebar = document.getElementById("sidebar");
+const menuButton = document.getElementById("drop-down");
+const addProject = document.getElementById("addProjectButton");
+const addNoteButton = document.getElementById("addNoteButton");
+const submitProjectButton = document.getElementById("submitProject");
+const submitNoteButton = document.getElementById("submitNote");
+const newToDo = document.getElementById("addToDoItem");
 
-menuButton.addEventListener('click', sidebarDisplay);
-addProject.addEventListener('click', addProj);
-newToDo.addEventListener('click', addProj)
-addNoteButton.addEventListener('click', addNote);
-submitNoteButton.addEventListener('click', submitNote);
-submitProjectButton.addEventListener('click', submitProject);
-submitProjectButton.addEventListener('click', displayMyProject);
+menuButton.addEventListener("click", sidebarDisplay);
+addProject.addEventListener("click", buildNewFolder);
+newToDo.addEventListener("click", addProj);
+//addNoteButton.addEventListener("click", addNote);     !!addNote function must be implemented later
+addNoteButton.addEventListener('click', buildNoteFolder)
+submitNoteButton.addEventListener("click", submitNote);
+submitProjectButton.addEventListener("click", submitProject);
+submitProjectButton.addEventListener("click", displayMyProject);
